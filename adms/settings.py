04fa3afaca6a9e62c2a43802c47f1e1c54822fd5
adms731 <advancemarketsolution@gmail.com>
2023-08-20 19:45:02 +0530

    bug fixed

--- adms/settings.py
+++ adms/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--7puakqlgy7!m2m#40@qxx6@65&r2_-i)ud*fetph2435n4$3t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
