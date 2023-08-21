da529b3c1fd0ee23680ca4959620756d9b0b6c77
adms731 <advancemarketsolution@gmail.com>
2023-08-21 18:25:44 +0530

    added the host for the custom domain

--- adms/settings.py
+++ adms/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure--7puakqlgy7!m2m#40@qxx6@65&r2_-i)ud*fetph2435n4$3t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'advancemarketsolution.com']
 
 
 # Application definition
